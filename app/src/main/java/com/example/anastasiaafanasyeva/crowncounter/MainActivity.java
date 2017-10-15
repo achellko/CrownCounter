cadc5c509fc1085ed9fe8ac79e47ad98b5be1f93
Anastasia Afanasyeva <nastya@afanasyev.kiev.ua>
2017-10-15 15:39:04 +0200

    fix counter value during rotations

--- app/src/main/java/com/example/anastasiaafanasyeva/crowncounter/MainActivity.java
+++ app/src/main/java/com/example/anastasiaafanasyeva/crowncounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.anastasiaafanasyeva.crowncounter;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mCrowsCounterButton;
     private int mCount = 0;
     private TextView mInfoTextView;
+    private static final String KEY_COUNT = "COUNT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         mCrowsCounterButton = (Button)findViewById(R.id.buttonCounter);
         mInfoTextView  = (TextView)findViewById(R.id.textView);
-        mCrowsCounterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mInfoTextView.setText("I count " + ++mCount + " crows");
-            }
-        });
+
+        if(savedInstanceState != null) {
+            mCount = savedInstanceState.getInt(KEY_COUNT, 0);
+            mInfoTextView.setText("Я насчитал " + mCount + " ворон");
+        }
+            mCrowsCounterButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mInfoTextView.setText("I count " + ++mCount + " crows");
+                }
+            });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(KEY_COUNT, mCount);
     }
 
     public void onClick(View view) {
